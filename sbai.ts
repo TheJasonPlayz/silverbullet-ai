66891b8e4b16288facee05f04af1ac08b2965d89
Jason Whitman <thejasonplayz@gmail.com>
2025-05-05 15:50:01 -0600

    Add prompting back

--- sbai.ts
+++ sbai.ts
@@ -1,31 +1,26 @@
-// import { editor, syscall } from "@silverbulletmd/silverbullet/syscalls";
-// import { generateText } from "npm:ai"
-// import { createOpenAI } from "npm:@ai-sdk/openai"
+import { editor, syscall } from "@silverbulletmd/silverbullet/syscalls";
+import { generateText } from "npm:ai"
+import { createOpenAI } from "npm:@ai-sdk/openai"
 import { configSchema, AIConfig } from "./src/configschema.ts"
-
-import { syscall } from "@silverbulletmd/silverbullet/syscalls"
 
 await syscall("config.define", "ai", configSchema)
 
-export async function getConfig() {
-    const conf = await syscall("config.get", "ai", "{}") as AIConfig
-    console.log(conf)
-}
+const conf = await syscall("config.get", "ai", "{}") as AIConfig
 
-// const provider = createOpenAI({
-//     baseURL: conf.baseURL,
-//     apiKey: conf.apiKey, //"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",
-//     compatibility: "compatible"
-// })
+const provider = createOpenAI({
+    baseURL: conf.baseURL,
+    apiKey: conf.apiKey, //"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",
+    compatibility: "compatible"
+})
 
-// export async function promptAI(): Promise<void> {
-//     const prompt = await editor.prompt("What would you like to ask me?")
+export async function promptAI(): Promise<void> {
+    const prompt = await editor.prompt("What would you like to ask me?")
     
-//     const { text } = await generateText({
-//         model: provider(conf.modelName),
-//         prompt: prompt,
-//         system: conf.systemPrompt
-//     })
+    const { text } = await generateText({
+        model: provider(conf.modelName),
+        prompt: prompt,
+        system: conf.systemPrompt
+    })
 
-//     return editor.insertAtCursor(text)
-// }
+    return editor.insertAtCursor(text)
+}
