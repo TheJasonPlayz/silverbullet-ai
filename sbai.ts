60f4fd97c7f0c42a7382f20f3f60d308addbddc5
Jason Whitman <thejasonplayz@gmail.com>
2025-05-05 14:38:46 -0600

    disable for testing

--- sbai.ts
+++ sbai.ts
@@ -4,22 +4,23 @@ import { createOpenAI } from "npm:@ai-sdk/openai"
 import { configSchema, AIConfig } from "./src/configschema.ts"
 
 await syscall("config.define", "ai", configSchema)
-const conf = await syscall("config.get", "ai") as AIConfig
+const conf = await syscall("config.get", "ai", "{}")
+console.log(conf)
 
-const provider = createOpenAI({
-    baseURL: conf.baseURL,
-    apiKey: conf.apiKey, //"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",
-    compatibility: "compatible"
-})
+// const provider = createOpenAI({
+//     baseURL: conf.baseURL,
+//     apiKey: conf.apiKey, //"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",
+//     compatibility: "compatible"
+// })
 
-export async function promptAI(): Promise<void> {
-    const prompt = await editor.prompt("What would you like to ask me?")
+// export async function promptAI(): Promise<void> {
+//     const prompt = await editor.prompt("What would you like to ask me?")
     
-    const { text } = await generateText({
-        model: provider(conf.modelName),
-        prompt: prompt,
-        system: conf.systemPrompt
-    })
+//     const { text } = await generateText({
+//         model: provider(conf.modelName),
+//         prompt: prompt,
+//         system: conf.systemPrompt
+//     })
 
-    return editor.insertAtCursor(text)
-}
+//     return editor.insertAtCursor(text)
+// }
