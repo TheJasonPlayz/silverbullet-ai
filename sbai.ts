efba9f5700a44136bb1ebb50879a505a5465ab57
Jason Whitman <thejasonplayz@gmail.com>
2025-05-05 14:31:13 -0600

    change model name to configurable

--- sbai.ts
+++ sbai.ts
@@ -5,7 +5,6 @@ import { configSchema, AIConfig } from "./src/configschema.ts"
 
 const conf = await syscall("config.define", "ai", configSchema) as AIConfig
 
-
 const provider = createOpenAI({
     baseURL: conf.baseURL,
     apiKey: conf.apiKey, //"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",
@@ -16,7 +15,7 @@ export async function promptAI(): Promise<void> {
     const prompt = await editor.prompt("What would you like to ask me?")
     
     const { text } = await generateText({
-        model: provider('deepseek/deepseek-chat-v3-0324:free'),
+        model: provider(conf.modelName),
         prompt: prompt,
         system: conf.systemPrompt
     })
