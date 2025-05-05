8546ad6d745e6b335b2732c77d6c2164a7365371
Jason Whitman <thejasonplayz@gmail.com>
2025-05-05 16:06:16 -0600

    try this

--- sbai.ts
+++ sbai.ts
@@ -1,22 +1,27 @@
 import { editor, syscall } from "@silverbulletmd/silverbullet/syscalls";
 import { generateText } from "npm:ai"
-import { createOpenAI } from "npm:@ai-sdk/openai"
+import { OpenAIProvider, createOpenAI } from "npm:@ai-sdk/openai"
 import { AIConfig } from "./src/configschema.ts"
 
 // await syscall("config.define", "ai", configSchema)
 
-const conf = await syscall("config.get", "ai", "{}") as AIConfig
-
-const provider = createOpenAI({
-    baseURL: conf.baseURL,
-    apiKey: conf.apiKey,
-    compatibility: "compatible"
-})
-
 export async function getConfig(): Promise<AIConfig> {
     return await syscall("config.get", "ai", "{}") as AIConfig
 }
+
+function makeProvider(conf: AIConfig): OpenAIProvider {
+    return createOpenAI({
+        baseURL: conf.baseURL,
+        apiKey: conf.apiKey,
+        compatibility: "compatible"
+    })
+}
+
 export async function promptAI(): Promise<void> {
+    const conf = await syscall("config.get", "ai", "{}") as AIConfig
+
+    const provider = makeProvider(conf)
+
     const prompt = await editor.prompt("What would you like to ask me?")
     
     const { text } = await generateText({
