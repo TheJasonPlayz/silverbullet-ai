df1471daa49a6e493c28268c3674c4397df05944
Jason Whitman <thejasonplayz@gmail.com>
2025-05-05 14:44:51 -0600

    Add get config function

--- sbai.ts
+++ sbai.ts
@@ -7,8 +7,11 @@ import { configSchema } from "./src/configschema.ts"
 import { syscall } from "@silverbulletmd/silverbullet/syscalls"
 
 await syscall("config.define", "ai", configSchema)
-const conf = await syscall("config.get", "ai", "{}")
-console.log(conf)
+
+export async function getConfig() {
+    const conf = await syscall("config.get", "ai", "{}")
+    console.log(conf)
+}
 
 // const provider = createOpenAI({
 //     baseURL: conf.baseURL,
