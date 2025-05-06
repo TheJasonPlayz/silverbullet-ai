d1f6d484b87cb48c2b6607758cef466d1870fb58
Jason Whitman <thejasonplayz@gmail.com>
2025-05-06 09:51:46 -0600

    Change to generateText and not streamText

--- sbai.ts
+++ sbai.ts
@@ -1,5 +1,5 @@
 import { editor, syscall } from "@silverbulletmd/silverbullet/syscalls";
-import { streamText } from "npm:ai"
+import { generateText, streamText } from "npm:ai"
 import { OpenAIProvider, createOpenAI } from "npm:@ai-sdk/openai"
 import { configSchema, AIConfig, defaultConfig } from "./src/configschema.ts"
 
@@ -8,7 +8,7 @@ async function defineConfig(schema: string): Promise<void> {
 }
 
 async function getConfig(): Promise<AIConfig> {
-    return await syscall("config.get", "ai", "{}") as AIConfig
+    return await syscall("config.get", "ai", defaultConfig) as AIConfig
 }
 
 export async function logConfig(): Promise<void> {
@@ -31,15 +31,16 @@ export async function promptAI(): Promise<void> {
 
     const prompt = await editor.prompt("What would you like to ask me?")
     
-    const { textStream } = await streamText({
+    const { text } = await generateText({
         model: provider(conf.modelName),
         prompt: prompt,
         system: conf.systemPrompt
     })
 
-    for await (const textPart of textStream) {
-        editor.insertAtPos(textPart, pos)
-    }
+    // for await (const textPart of textStream) {
+    //     editor.insertAtPos(textPart, pos)
+    // }
+    editor.insertAtPos(text, pos)
 }
 
 // await defineConfig(configSchema)
